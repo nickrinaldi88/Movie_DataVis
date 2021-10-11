827a1d96f6b3dcc2229b76aab9d06dcf1884ad09
Nick <rinaldinick88@gmail.com>
2021-10-10 22:47:35 -0400

    change

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ d3.csv("out.csv").then(function (data){
     // map creates a new array populae with results of calling provided funciton 
 
     //  //   console.log(csv);
-    let current_data = {};
+    var current_data = {};
     let parse = d3.timeParse("%Y-%m-%d");
 
     let bros = ['Derek',
@@ -45,109 +45,109 @@ d3.csv("out.csv").then(function (data){
 
   for (let j = 0; j < bros.length; j++) {
 
+
       for (let i = 0; i < data.length; i++) {
         // line of data
         let item = data[i];
 
-        let movie = item['Movie']
-        let picked = item['Picked by:']
-        let viewer = bros[j]
-        let viewer_rating = item[bros[j]]
-        console.log("row " + String(i));
-        console.log("Movie: " + String(item['Movie']));
-        console.log("Picked By: " + String(item['Picked by:']));
-        console.log("Viewer: " + String(bros[j]));
-        console.log("Viewer Rating: "  + String(item[bros[j]]));        // console.log(typeof(item))
-        // console.log(item.Derek);
-        // console.log(bros[j]);
-        // console.log(item.bros[j]);
-    }
+        let film = item['Movie']
+        var picked = item['Picked by:']
+        let bro = bros[j]
+        let bro_rating = item[bros[j]]
+        let date = item['Date']
+
+      if (!current_data[bro]){
+        current_data[bro] = [];
+    };
+
+
+          current_data[bro].push({
+          picked_by: picked,
+          movie: film,
+          // viewer: bro,
+          viewer_rating: bro_rating,
+          date: date
+        });
+
+      
+      };
   
+    };
+
+    var svg = d3
+    .select("#main-div")
+    .classed("svg-container", true)
+    .append("svg")
+    // .attr("preserveAspectRatio", "xMinYMin meet")
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
+    .attr("viewBox", "0 0 1400 500")
+    .append("g")
+    .attr("transform", "translate(80, 20)");
     
-    }
-
-    // for (let i = 0; i < data.length; i++) {
-    //   let item = data[i];
-      // if (!current_data[item.]) current_data[item.Word] = [];
-
-    // current_data[bros[j]].push({
-    //   date: parse(item.Date),
-    //   rating: item.value,
-    //   pickedBy: item.  
-
-  
-  
-  
-  
-  
-  
-  //   }
-
-  // for (let i = 0; i < data.length; i++) {
-  //     let item = data[i];
-  //     if (!current_data[item.]) current_data[item.Word] = [];
-
-  //   current_data[item.Word].push({
-  //     date: parse(item.Date),
-  //     count: item.Count,
-  //   });
-  // }
-
-
-
-
-
-
-
-
-
-    // let rangeOfbros = [9, 20]
-    // let bros =  {rating:,
-    //              movie_name: data['Movie'],
-    //              Picked_by: data['Picked by:'],
-                //  date: data['Date']}
-
-    console.log(data.columns('Picked by:'))
-    let series = data.columns.slice(9, 20).map(key => data.map(({[key]: value,  date}) => ({key, date, value})))
-
-
-
-
-    const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height]);
 
     const serie = svg.append("g")
     .selectAll("g")
-    .data(series)
+    .data(current_data)
     .join("g");
+
+    ratings = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    console.log(current_data)
 
     margin = ({top: 30, right: 50, bottom: 30, left: 30})
 
+    first_select = d3.select("#select1")
+      .selectAll("option")
+      .data(Object.keys(current_data))
+      .enter()
+      .append("option")
+      .text(function (d){
+        return d});
+
+
+    second_select = d3.select("#select2")
+      .selectAll("option")
+      .data(Object.keys(current_data))
+      .enter()
+      .append("option")
+      .text(function (d){
+        return d
+      });
+
+
     xAxis = g => g
         .attr("transform", `translate(0,${height - margin.bottom})`)
+
+    
 
     x = d3.scaleUtc()
         .domain([data[0].date, data[data.length - 1].date])
         .range([margin.left, width - margin.right])
 
     y = d3.scaleLinear()
-        .domain([0, d3.max(series, s => d3.max(s, d => d.value))])
+        .domain([0, 10])
         .range([height - margin.bottom, margin.top])
 
-    z = d3.scaleOrdinal(data.columns.slice(1), d3.schemeCategory10)
+    z = d3.scaleOrdinal(data.columns.slice(9, 20), d3.schemeCategory10)
+
+    var xAxis = d3.axisBottom().scale(x)
+    var yAxis = d3.axisLeft().scale(y);
 
     svg.append("g")
         .call(xAxis) // call x axis
+
+    console.log(serie);
     
-    serie.append("path")
+    current_data.append("path")
           .attr("fill", "none")
           .attr("stroke", d => z(d[0].key))
           .attr("stroke-width", 1.5)
           .attr("d", d3.line()
               .x(d => x(d.date))
-              .y(d => y(d.value)));
+              .y(d => y(d.Derek)));
     
-    serie.append("g")
+    current_data.append("g")
           .attr("font-family", "sans-serif")
           .attr("font-size", 10)
           .attr("stroke-linecap", "round")
